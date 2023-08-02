cf40557aeee4560a986851940ce162755190d94e
kharvirakesh <kharvirakesh3@gmail.com>
2023-08-02 14:34:15 +0530

    FEAT : node routing added

--- complete-node-bootcamp-master/1-node-farm/starter/txt/index.js
+++ complete-node-bootcamp-master/1-node-farm/starter/txt/index.js
@@ -1,16 +1,28 @@
-const fs = require('fs');
-const http = require('http');
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
 
-const hostname = '127.0.0.1';
+const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    console.log(req);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World!');
+  // console.log(req);
+  console.log(req.url);
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+//   res.end("Hello World!");
+
+  const pathname = req.url;
+
+  if (pathname === "/" || pathname === "/overview") {
+    res.end("This is HomePage");
+  } else if (pathname === "/project") {
+    res.end("This is Product Page");
+  } else {
+    res.end("page not found");
+  }
 });
 
 server.listen(port, hostname, () => {
-    console.log(`Server listening at http://${hostname}:${port}/`);
+  console.log(`Server listening at http://${hostname}:${port}/`);
 });
