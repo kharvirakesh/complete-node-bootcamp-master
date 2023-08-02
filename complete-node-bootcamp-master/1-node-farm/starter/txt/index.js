1ac3dd42963c6e72e0e95388060db26a14e7b8a4
kharvirakesh <kharvirakesh3@gmail.com>
2023-08-02 15:23:18 +0530

    FEAT : integrated airbnb coding standard

--- complete-node-bootcamp-master/1-node-farm/starter/txt/index.js
+++ complete-node-bootcamp-master/1-node-farm/starter/txt/index.js
@@ -1,28 +1,30 @@
 const fs = require("fs");
-const http = require("http");
 const url = require("url");
+const http = require("http");
 
 const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  // console.log(req);
-  console.log(req.url);
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-//   res.end("Hello World!");
+    console.log(req.url);
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
 
-  const pathname = req.url;
+    const pathname = req.url;
 
-  if (pathname === "/" || pathname === "/overview") {
-    res.end("This is HomePage");
-  } else if (pathname === "/project") {
-    res.end("This is Product Page");
-  } else {
-    res.end("page not found");
-  }
+    if (pathname === "/" || pathname === "/overview") {
+        res.end("This is HomePage");
+    } else if (pathname === "/project") {
+        fs.readFileSync('./dev-data/data.json');
+        res.end("This is Product Page");
+    } else {
+        res.writeHead(404, {
+            'Content-Type': 'text/html',
+        })
+        res.end('<h1>page not found</h1>');
+    }
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server listening at http://${hostname}:${port}/`);
+    console.log(`Server listening at http://${hostname}:${port}/`);
 });
